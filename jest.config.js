383ec49f71ebc0b78b1098cd686a90a3ea3f2052
Egor Zorin <zorinj@gmail.com>
2020-07-20 17:20:29 +0300

    Add tests

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  transform: {
+    "^.+\\.ts?$": "ts-jest",
+  },
+  moduleNameMapper: {
+    "@tests(.*)$": "<rootDir>/tests/$1",
+    "^@/(.*)": "<rootDir>/src/$1"
+  },
+  verbose: true
+};
