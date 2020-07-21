a5851c129638d59d3d3377c540790a464421c804
Egor Zorin <zorinj@gmail.com>
2020-07-21 20:05:59 +0300

    Add: env variables

--- src/demo.ts
+++ src/demo.ts
@@ -7,7 +7,7 @@ if (module.hot) {
 (async () => {
   const suggestions = new Suggestions(
     new DadataService({
-      apiKey: "222e2fe15c60358261e2e17bc9dbc7687b0b34ee"
+      apiKey: process.env.DADATA_API_KEY
     })
   );
 
