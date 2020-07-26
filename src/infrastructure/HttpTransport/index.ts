45070ff7397dcf96c666a0f414e5b6a58586624e
Egor Zorin <zorinj@gmail.com>
2020-07-26 20:59:16 +0300

    Add: export interfaces

--- src/infrastructure/HttpTransport/index.ts
+++ src/infrastructure/HttpTransport/index.ts
@@ -1,2 +1,4 @@
 export { default as HttpTransport } from "./HttpTransport";
 export { default as AxiosClient } from "./AxiosClient";
+export { default as CJHttpTransport } from "./CJHttpTransport.interface";
+export { default as CJResponseFormat } from "./CJResponseFormat.interface";
