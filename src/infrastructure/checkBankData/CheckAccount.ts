dc328ef0d2e2f3c7e6f1b02ae90b5bb2babe72eb
Egor Zorin <zorinj@gmail.com>
2021-07-02 07:45:35 +0300

    Add: checking account

--- src/infrastructure/checkBankData/CheckAccount.ts
+++ src/infrastructure/checkBankData/CheckAccount.ts
@@ -1,14 +1,18 @@
 export class CheckAccount {
 	private weightCoefficients: Array<number> = [7,1,3,7,1,3,7,1,3,7,1,3,7,1,3,7,1,3,7,1,3,7,1];
 	private account: string
+	private weightCoefficientsSize: number
 
 	constructor (account: string) {
 		this.account = account
+		this.weightCoefficientsSize = this.weightCoefficients.length
 	}
 
 	private calculate (): number {
+		if (isNaN(Number(this.account))) throw new Error('Account is not a number')
+
 		let sum = 0
-		for (let i: number = 0; i <= 22; i++) {
+		for (let i: number = 0; i < this.weightCoefficientsSize; i++) {
 			sum = sum + ( Number(this.account.charAt(i)) * this.weightCoefficients[i] ) % 10;
 		}
 		return sum
